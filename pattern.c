57f53d024ba533e42963c2db042e3bb1e8fbff36
__alokkarn__ <alokkyasth@gmail.com>
2022-06-06 23:07:01 +0530

    clang

---
+++ pattern.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void main()
+
+{
+    int i,j,k=1;
+    for(i=1;i<=5;i++)
+    {
+        for(j=1;j<=i;j++)
+        {
+            if(k%2==0)
+            {
+                printf("0");
+            }
+            else
+            {
+                printf("1");
+            }
+            k++;
+        }
+        printf("\n");
+    }
+    
+}
